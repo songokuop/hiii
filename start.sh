45eb07b7b1dfb3ec31208219a550c3fe7bf1b3ab
mdsaif572 <143118236+mdsaif572@users.noreply.github.com>
2023-11-27 17:37:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mdsaif572/Elsa.got /Elsa
+  git clone https://github.com/mdsaif572/Elsa.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
