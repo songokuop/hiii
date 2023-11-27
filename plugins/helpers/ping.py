d4c0296d8a5742b0b068c571a030d5cc2a93b902
mdsaif572 <143118236+mdsaif572@users.noreply.github.com>
2023-11-27 17:31:01 +0530

    Initial commit

---
+++ plugins/helpers/ping.py
@@ -0,0 +1,32 @@
+"""Telegram Ping / Pong Speed
+Syntax: .ping"""
+
+import time
+import random
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# -- Constants -- #
+ALIVE = "<b>Not dead but still here.. You have no love for me now.  Good.. You are not the same as you were before..</b>" 
+# -- Constants End -- #
+
+
+@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+async def check_alive(_, message):
+    await message.reply_text(ALIVE)
+
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"<b>Pong!\n{time_taken_s:.3f} ms\n\n©ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ™</b>")
+
+
+
+
+
+
---
+++ sample_info.py
@@ -0,0 +1,25 @@
+# Bot information
+SESSION = ''
+USER_SESSION = ''
+API_ID = 
+API_HASH = ''
+BOT_TOKEN = ''
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [, 'terminator']
+CHANNELS = [, '']
+AUTH_USERS = []
+AUTH_CHANNEL = 
+
+# MongoDB information
+DATABASE_URI = ""
+DATABASE_NAME = ''
+COLLECTION_NAME = ''  # If you are using the same database, then use different collection name for each bot
+LOG_CHANNEL = ''
+
+
